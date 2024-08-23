6cd3879832018749589ae66eefbd13d17500d5ee
gomezzero <gomezjuanzero@gmail.com>
2024-08-23 07:43:47 -0500

    feature: Creating the Role Attribute

--- Models/Book.cs
+++ Models/Book.cs
@@ -7,17 +7,15 @@ namespace Troja.Models;
 public class Book
 {
     public int BookId { get; set; } // Clave primaria
-    // public string LocationCode { get; set; }
     public string Title { get; set; }
     public int AuthorId { get; set; } // Clave foránea
     public string LiteraryGenre { get; set; }
     public DateOnly PublicationDate { get; set; }
     public string ISBN { get; set; }
     public string StatusValue { get; set; }
-
+    
     // Propiedades de navegación
     public Author? Author { get; set; }
-    // public Location Location { get; set; }
     public required ICollection<Loan>? Loans { get; set; }
 
 }
