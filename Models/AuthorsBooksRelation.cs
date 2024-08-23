2d0fffe6caa3b44ad1cd7fa2a83fe588e35f9dd1
gomezzero <gomezjuanzero@gmail.com>
2024-08-23 07:26:46 -0500

    feature: Creating AuthorsBooksRelation Class Attributes

--- Models/AuthorsBooksRelation.cs
+++ Models/AuthorsBooksRelation.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Troja.Models
+namespace Troja.Models;
+public class AuthorsBooksRelation
 {
-    public class AuthorsBooksRelation
-    {
-        
-    }
+    public int Id { get; set; }
+
+    public int AuthorId { get; set; }
+
+    public int BookId { get; set; }
 }
