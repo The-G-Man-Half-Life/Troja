bde88bf576c39d011b1e097731f4562808f1b87f
gomezzero <gomezjuanzero@gmail.com>
2024-08-23 07:28:00 -0500

    feature: Creating ReleaseViewModel Class Attributes

--- Models/ReleaseViewModel.cs
+++ Models/ReleaseViewModel.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Troja.Models
+namespace Troja.Models;
+public class ReleaseViewModel
 {
-    public class ReleaseViewModel
-    {
-        
-    }
+    public int LoanId { get; set; }
+    public string BookTitle { get; set; }
+    public int StatusValue { get; set; }
 }
