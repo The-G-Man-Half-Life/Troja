86add19a1afe8eb73199ec4901711f04c9997512
gomezzero <gomezjuanzero@gmail.com>
2024-08-23 07:26:03 -0500

    feature: Creating Book Class Attributes

--- Models/Book.cs
+++ Models/Book.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Troja.Models
+namespace Troja.Models;
+public class Book
 {
-    public class Book
-    {
-        
-    }
+    public int BookId { get; set; } // Clave primaria
+    // public string LocationCode { get; set; }
+    public string Title { get; set; }
+    public int AuthorId { get; set; } // Clave foránea
+    public string LiteraryGenre { get; set; }
+    public DateOnly PublicationDate { get; set; }
+    public string ISBN { get; set; }
+    public string StatusValue { get; set; }
+
+    // Propiedades de navegación
+    public Author? Author { get; set; }
+    // public Location Location { get; set; }
+    public required ICollection<Loan>? Loans { get; set; }
+
 }
