128677fb04af2eaf6ba088e810031b69d69f60fb
gomezzero <gomezjuanzero@gmail.com>
2024-08-23 07:27:24 -0500

    feature: Creating LoanViewModel Class Attributes

--- Models/LoanViewModel.cs
+++ Models/LoanViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Troja.Models
+namespace Troja.Models;
+public class LoanViewModel
 {
-    public class LoanViewModel
-    {
-        
-    }
+    public int LoanId { get; set; }
+    public string BookTitle { get; set; }
+    public string UserFullName { get; set; }
+    public DateTime? DevolutionDate { get; set; }
+    public int StatusValue { get; set; }
 }
