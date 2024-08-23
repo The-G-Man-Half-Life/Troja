2d1dbefcdc76a67cae37dcc92ce715db4063ce6d
jtorova5 <jtorova5@gmail.com>
2024-08-23 10:00:38 -0500

    feat: adding AuthorsController

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,175 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Troja.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using Troja.Data;
+
+namespace Troja.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AuthorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Authors
+        public async Task<IActionResult> Index()
+        {
+            var authors = await _context.Authors.ToListAsync();
+            return View(authors);
+        }
+
+        // GET: Authors/Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var author = await _context.Authors
+                .Include(a => a.Books)
+                .FirstOrDefaultAsync(m => m.AuthorId == id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return View(author);
+        }
+
+        // GET: Authors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Authors/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AuthorName,AuthorLastName")] Author author)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(author);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                Console.Write("Error");
+            }
+            // return RedirectToAction(nameof(Index));
+            return View(author);
+        }
+
+        // GET: Authors/Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var author = await _context.Authors.FindAsync(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            return View(author);
+        }
+
+        // POST: Authors/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AuthorId,AuthorName,AuthorLastName")] Author author)
+        {
+            if (id != author.AuthorId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(author);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AuthorExists(author.AuthorId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(author);
+        }
+
+        // GET: Authors/Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var author = await _context.Authors
+                .Include(a => a.Books)
+                .FirstOrDefaultAsync(m => m.AuthorId == id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return View(author);
+        }
+
+        // POST: Authors/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+            if (author != null)
+            {
+                _context.Authors.Remove(author);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AuthorExists(int id)
+        {
+            return _context.Authors.Any(e => e.AuthorId == id);
+        }
+
+        // GET: Authors/BooksByAuthor
+        public async Task<IActionResult> BooksByAuthor(int authorId)
+        {
+            var author = await _context.Authors
+                .Include(a => a.Books)
+                .FirstOrDefaultAsync(a => a.AuthorId == authorId);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return View(author);
+        }
+    }
+}
