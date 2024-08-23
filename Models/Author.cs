62afbb4f3de33286cfee77069c498ff135231f3c
gomezzero <gomezjuanzero@gmail.com>
2024-08-23 07:22:03 -0500

    feature: creating empty basic classes

---
+++ Models/Author.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+namespace Troja.Models;
+public class Author
+{
+
+}
