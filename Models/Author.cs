a10811aa59c80ef1dbd1e6f9ad209a1669437b41
gomezzero <gomezjuanzero@gmail.com>
2024-08-23 07:24:14 -0500

    feature: Creating Author Class Attributes

--- Models/Author.cs
+++ Models/Author.cs
@@ -7,5 +7,20 @@ using System.Threading.Tasks;
 namespace Troja.Models;
 public class Author
 {
+    public int AuthorId { get; set; }
 
+    [Required(ErrorMessage = "Author Name is required.")]
+    public string AuthorName { get; set; }
+    [Required(ErrorMessage = "Author Last Name is required.")]
+    public string AuthorLastName { get; set; }
+    // Propiedad de navegación para los libros
+    public ICollection<Book>? Books { get; set; }
+
+    // Constructor for Author 
+    public Author()
+    {
+
+        // Books = new List<Book>();
+
+    }
 }
